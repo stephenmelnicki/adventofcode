62f03ebb656db36c3d0360c1648fcdafb7f62d3b
Stephen Melnicki <stephen@stephenmelnicki.com>
2023-01-04 19:18:47 -0800

    day7: no space left on device (initial commit)

---
+++ 2022/day7/index.ts
@@ -0,0 +1,11 @@
+import path from "path";
+import fs from "fs";
+
+function main() {
+  const input: string = fs.readFileSync(path.join(__dirname, "input.txt"), {
+    encoding: "utf8",
+  });
+
+  console.log("--- Day 7: No Space Left On Device ---\n");
+  console.log("NOT YET IMPLEMENTED");
+}
